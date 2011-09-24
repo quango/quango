5f6dd720f9baa400a9321c27b90061469ab5a470
Simon Probert <simon.probert1@googlemail.com>
2011-09-24 23:21:43 +1000

    x

--- config/initializers/dragonfly_images.rb
+++ config/initializers/dragonfly_images.rb
@@ -4,13 +4,16 @@ require 'watermark'
 
 app = Dragonfly[:images]
 
+app.configure do |c|
+  c.convert_command = "/usr/local/bin/convert"          # defaults to "convert"
+  c.identify_command = "/usr/local/bin/convert"         # defaults to "convert"
+  c.log_commands = false                                 # defaults to false
+end
+
 app.configure_with(:rmagick)
 app.configure_with(:rails)
-#app.configure do |c|
-#  c.convert_command = "/usr/local/bin/convert"          # defaults to "convert"
-#  c.identify_command = "/usr/local/bin/convert"         # defaults to "convert"
-#  c.log_commands = false                                 # defaults to false
-#end
+
+
 
 
 
