f65e65f94e246af50f17efca7f5a3bb64a85a94c
Stephen Sullivan <steve.t.sullivan@gmail.com>
2012-01-05 22:32:12 -0500

    Added Dragonfly processor with desat, negate and watermark

--- config/initializers/dragonfly_images.rb
+++ config/initializers/dragonfly_images.rb
@@ -1,6 +1,7 @@
 #require 'dragonfly/rails/images'
 require 'rack/cache'
-#require 'watermark' 
+#require 'watermark'
+require 'image_processor'
 
 app = Dragonfly[:images]
 
@@ -11,8 +12,8 @@ app.configure_with(:imagemagick)
 app.configure_with(:rails)
 
 
-app.configure{|c|Dragonfly::ImageMagickUtils.convert_command = '/usr/local/bin/convert'}
-app.configure{|c|Dragonfly::ImageMagickUtils.identify_command = '/usr/local/bin/identify'}
+app.configure{|c|Dragonfly::ImageMagickUtils.convert_command = '/usr/bin/convert'}
+app.configure{|c|Dragonfly::ImageMagickUtils.identify_command = '/usr/bin/identify'}
 
 
 #app.convert_command = "/usr/local/bin/convert"          # defaults to "convert"
@@ -25,7 +26,8 @@ app.configure{|c| c.url_host = ''} # 'http://think.it' }
 app.define_macro_on_include(MongoMapper::Document, :image_accessor)
 app.define_macro_on_include(MongoMapper::EmbeddedDocument, :image_accessor)
 
-app.processor.register(Dragonfly::Processing::Watermark) 
+app.processor.register(Dragonfly::Processing::Watermark)
+app.processor.register(Dragonfly::Processing::ImageProcessor)
 
 # Where the middleware is depends on the version of Rails
 middleware = Rails.respond_to?(:application) ? Rails.application.middleware : ActionController::Dispatcher.middleware
